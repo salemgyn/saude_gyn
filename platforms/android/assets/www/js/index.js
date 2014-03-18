35b1d8311ba4bf331f88a5640ff07667da32bedb
Melky-Salem Fernandes Batista da Silva <msalem@globo.com>
2014-03-18 14:39:16 -0300

    Adicionado metodo de adição de markers por aja

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,30 +1,81 @@
-//Espere o Phonegap Carregar
-var map;
-var mapOptions;
-var marker;
-function onSuccessPosition(position){
-	map.setCenter(new google.maps.LatLng(position.coords.latitude,position.coords.longitude));
-	marker.setPosition(new google.maps.LatLng(position.coords.latitude,position.coords.longitude));
-}
-function onErrorPosition(error){
-	alert("Erro!\nCodigo: "+error.code+"\nMensagem: "+error.message);
-}
-document.addEventListener("deviceready", onDeviceReady, false);
-function onDeviceReady() {
-	$("#btnTema").click(function(){
-		$(".app").removeClass("ui-page-theme-a").addClass("ui-page-theme-b");
-	});
-	$(document).delegate("#mapPage", "pageshow", function() {
-		$("#divMap").css("height",parseInt($(window).height())*0.95); //0.95 = 95% of the viewport to display the map		
-		map = new google.maps.Map(document.getElementById("divMap"),{zoom: 16, center: new google.maps.LatLng(-16.6958759,-49.3042674), mapTypeId: google.maps.MapTypeId.ROADMAP})
-		marker = new google.maps.Marker({
-			position: new google.maps.LatLng(-16.6958759,-49.3042674),
-			map: map
-		});
-		navigator.geolocation.getCurrentPosition(onSuccessPosition,onErrorPosition);
-		navigator.geolocation.watchPosition(onSuccessPosition,onErrorPosition,{ maximumAge: 3000, enableHighAccuracy: true });
-	});
-	window.addEventListener("orientationchange",function(){
-		$("#divMap").css("height",parseInt($(window).height())*0.95);		
-	});
-}
+var App = {
+    "app_loaded": false,
+    "testing_on_desktop": true,
+ 	"init":function(){
+		var map;
+		var mapOptions;
+		var marker;
+		console.log("[init]");
+	 
+	    if (document.URL.indexOf("http://") === -1) {
+	        App.testing_on_desktop = false;
+	    }
+	 
+	    jQuery(document).ready(function () {
+	        console.log("jQuery finished loading");
+	 
+	        if (App.testing_on_desktop) {
+	            console.log("PhoneGap finished loading");
+	            _onDeviceReady();
+	        } else {
+	            document.addEventListener("deviceReady", function () {
+	                console.log("PhoneGap finished loading");
+	                _onDeviceReady();
+	            }, false);
+	        }
+	 
+	        jQuery(document).one("pageinit", function () {
+	            console.log("jQuery.Mobile finished loading");
+	        });
+	 
+	        console.log("PhoneGap & jQuery.Mobile finished loading");
+	        initPages();
+	        console.log("App finished loading");
+	        App.app_loaded = true;
+	    });
+	 
+	    function _onDeviceReady () {
+			console.log("[_onDeviceReady]");
+			jQuery(document).delegate("#mapPage", "pageshow", function() {
+				jQuery("#divMap").css("height",parseInt(jQuery(window).height())*0.95); //0.95 = 95% of the viewport to display the map		
+				map = new google.maps.Map(document.getElementById("divMap"),{zoom: 16, center: new google.maps.LatLng(-16.6958759,-49.3042674), mapTypeId: google.maps.MapTypeId.ROADMAP})
+				marker = new google.maps.Marker({
+					position: new google.maps.LatLng(-16.6958759,-49.3042674),
+					map: map
+				});
+				navigator.geolocation.getCurrentPosition(onSuccessPosition,onErrorPosition);
+				navigator.geolocation.watchPosition(onSuccessPosition,onErrorPosition,{ maximumAge: 3000, enableHighAccuracy: true });
+				jQuery.ajax({
+					url: "ajax/farmacias.json",
+					type: "get",
+					success: function(data){
+						var farmacia;
+						for(farmacia in data.farmacias){
+							alert(farmacia.latitude+","+farmacias.longitude);
+							var m = new google.maps.Maker({
+								position: new google.maps.LatLng(farmacia.latitude,farmacias.longitude),
+								map: map
+							});
+						}
+					}
+				});
+			});
+			window.addEventListener("orientationchange",function(){
+				jQuery("#divMap").css("height",parseInt(jQuery(window).height())*0.95);		
+			});
+	    };
+	    function initPages () {
+	    };	    
+		function onSuccessPosition(position){
+		console.log("[onSuccessPosition]");
+			map.setCenter(new google.maps.LatLng(position.coords.latitude,position.coords.longitude));
+			marker.setPosition(new google.maps.LatLng(position.coords.latitude,position.coords.longitude));
+		};
+		function onErrorPosition(error){
+		console.log("[onErrorPosition]");
+			alert("Erro!\nCodigo: "+error.code+"\nMensagem: "+error.message);
+		};
+	}, 
+    "utilities": {
+    },
+};
--- www/js/index.js
+++ www/js/index.js
@@ -1,15 +1,13 @@
 var App = {
-	"map":"",
-	"mapOptions":"",
-	"marker":"",
-	"onSuccessPosition":function(){
-		map.setCenter(new google.maps.LatLng(position.coords.latitude,position.coords.longitude));
-		marker.setPosition(new google.maps.LatLng(position.coords.latitude,position.coords.longitude));
-	},
-	"onErrorPosition":function(){
-		alert("Erro!\nCodigo: "+error.code+"\nMensagem: "+error.message);
-	},
-	"init":function(){
+    "app_loaded": false,
+    "testing_on_desktop": true,
+    "marker":null,
+    "markerArray":[],
+    "map":null,
+ 	"init":function(){
+		var map;
+		var mapOptions;
+		var marker;
 		console.log("[init]");
 	 
 	    if (document.URL.indexOf("http://") === -1) {
@@ -40,25 +38,60 @@ var App = {
 	    });
 	 
 	    function _onDeviceReady () {
-	    	jQuery("#btnTema").click(function(){
-		    	jQuery("#btnTema").click(function(){
-				jQuery(".app").removeClass("ui-page-theme-a").addClass("ui-page-theme-b");
-			});
+			console.log("[_onDeviceReady]");
 			jQuery(document).delegate("#mapPage", "pageshow", function() {
 				jQuery("#divMap").css("height",parseInt(jQuery(window).height())*0.95); //0.95 = 95% of the viewport to display the map		
-				map = new google.maps.Map(document.getElementById("divMap"),{zoom: 16, center: new google.maps.LatLng(-16.6958759,-49.3042674), mapTypeId: google.maps.MapTypeId.ROADMAP})
-				marker = new google.maps.Marker({
+				App.map = new google.maps.Map(document.getElementById("divMap"),{zoom: 16, center: new google.maps.LatLng(-16.6958759,-49.3042674), mapTypeId: google.maps.MapTypeId.ROADMAP})
+				App.marker = new google.maps.Marker({
 					position: new google.maps.LatLng(-16.6958759,-49.3042674),
-					map: map
+					map: App.map,
+					icon: "img/seta-local.png"
 				});
 				navigator.geolocation.getCurrentPosition(onSuccessPosition,onErrorPosition);
 				navigator.geolocation.watchPosition(onSuccessPosition,onErrorPosition,{ maximumAge: 3000, enableHighAccuracy: true });
+				jQuery.ajax({
+					url: "ajax/farmacias.json",
+					type: "get",
+					success: function(data){
+						jQuery.each(data.farmacias, function(i,val){
+							var m = new google.maps.Marker({
+								position: new google.maps.LatLng(val.latitude,val.longitude),
+								title: val.title,
+								map: App.map
+							});
+							google.maps.event.addListener(m, 'click', function() {
+						        var infoWindow = new google.maps.InfoWindow({
+						        	content: "<b>"+val.title+"</b>"
+						        });
+						        infoWindow.open(App.map,m)
+						    });
+							App.markerArray.push(m);
+						});
+					},
+					error: function(error){
+						alert("Erro: "+error.message);
+					}
+				});
 			});
 			window.addEventListener("orientationchange",function(){
 				jQuery("#divMap").css("height",parseInt(jQuery(window).height())*0.95);		
 			});
 	    };
+	    var infowindow = new google.maps.InfoWindow({
+		    size: new google.maps.Size(150, 50)
+		});
 	    function initPages () {
-	    };
-	},
-}
+	    };	    
+		function onSuccessPosition(position){
+			console.log("[onSuccessPosition]");
+			map.setCenter(new google.maps.LatLng(position.coords.latitude,position.coords.longitude));
+			marker.setPosition(new google.maps.LatLng(position.coords.latitude,position.coords.longitude));
+		};
+		function onErrorPosition(error){
+			console.log("[onErrorPosition]");
+			alert("Erro!\nCodigo: "+error.code+"\nMensagem: "+error.message);
+		};
+	}, 
+    "utilities": {
+    },
+};
